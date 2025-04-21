bce718c628ec14357b15cf4bc47f663630ecca2d
NinerMG <maciej.gala@hotmail.com>
2025-04-21 21:00:15 +0200

    code refactoring

--- bakcend_version/equal_chances.py
+++ bakcend_version/equal_chances.py
@@ -1,25 +1,45 @@
 import random
 
-options = ["rock", "paper" , "scissors"]
+options = ["rock", "paper", "scissors"]
 wins_options = {"rock": "paper",
-                    "paper": "scissors",
-                    "scissors": "rock"}
+                "paper": "scissors",
+                "scissors": "rock"}
+
+def get_user_choice():
+    choice = input("Enter your choice rock/paper/scissors\n")
+    return choice
+
+def get_computer_choice():
+    return random.choice(options)
+
+def determine_winner(user, computer):
+    if user == computer:
+        return "draw"
+    elif wins_options[user] == computer:
+        return "win"
+    else:
+        return "lose"
+
+def print_result(result, computer_choice):
+    if result == "draw":
+        print(f"There is a draw ({computer_choice})")
+    elif result == "win":
+        print(f"Well done. The computer chose {computer_choice} and failed")
+    else:
+        print(f"Sorry, but the computer chose {computer_choice}")
 
 def game():
     while True:
-        user_choice = input("Enter your choice rock/paper/scissors\n")
+        user_choice = get_user_choice()
         if user_choice == "!exit":
             print("Bye!")
             break
         if user_choice not in options:
             print("Wrong input, try again!")
-        else:
-            computer_choice = random.choice(options)
+            continue
 
-            if user_choice == computer_choice:
-                print(f"There is a draw ({computer_choice})")
-            elif wins_options[user_choice] == computer_choice:
-                print(f"Well done. The computer chose {computer_choice} and failed")
-            else:
-                print(f"Sorry, but the computer chose {computer_choice}\n")
+        computer_choice = get_computer_choice()
+        result = determine_winner(user_choice, computer_choice)
+        print_result(result, computer_choice)
+
 game()
